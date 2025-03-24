114336b7a7e8687f662d0edd27f44562ea16446a
Adam Fanello <adam.fanello@rackspace.com>
2025-03-24 13:46:20 -0700

    Modernize Cargoplane library
    Ex-out example API url to make it clearer that it still needs to be filled in.

--- demo/client/ng-cargoplane-demo/src/app/environment.ts
+++ demo/client/ng-cargoplane-demo/src/app/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // TODO: Change to your deployed API Gateway
-  apiUrl: 'https://wos2axy459.execute-api.us-west-2.amazonaws.com/'
+  apiUrl: 'https://xxxxxxxxxx.execute-api.xx-xxxx-x.amazonaws.com/'
 };
